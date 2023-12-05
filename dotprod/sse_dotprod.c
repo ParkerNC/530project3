b6137280ecf67575f82f450733327bbd931a16a4
string <pcollie4@vols.utk.edu>
2023-12-04 22:17:40 -0500

    code working

--- dotprod/sse_dotprod.c
+++ dotprod/sse_dotprod.c
@@ -3,30 +3,25 @@
 float dotprod(float x[], float y[], int n)
 {
 
-    __m128 xvalues, yvalues, loopsum;
+    __m128 yvalues, xvalues, loopsum;
 
-    float loopx[4] __attribute__ ((aligned (16)));
-    float loopy[4] __attribute__ ((aligned (16)));
+    float tmp[4];
 
-    float zero = 0;
-    loopsum = _mm_load1_ps(&zero);
+    loopsum = _mm_setzero_ps();
     
-    int i, j;
+    int i;
 
     for (i = 0; i < n; i+=4){
-        for (j = 0; j < 4; j++){
-            loopx[j] = x[i+j];
-            loopy[j] = y[i+j];
-        }
-        xvalues = _mm_load_ps(&loopx[0]);
-        yvalues = _mm_load_ps(&loopy[0]);
+
+        xvalues = _mm_load_ps(&x[i]);
+        yvalues = _mm_load_ps(&y[i]);
 
         loopsum = _mm_add_ps(loopsum, _mm_mul_ps(xvalues, yvalues));
 
     }
 
-    _mm_store_ps(&xvalues, loopsum);
+    _mm_store_ps(tmp, loopsum);
 
-    return xvalues[0] + xvalues[1] + xvalues[2] + xvalues[3];
+    return tmp[0] + tmp[1] + tmp[2] + tmp[3];
 
 }
